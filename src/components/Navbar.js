986740cbce8da148f345d6a02868c46b31903951
Wenth93 <64720186+Wenth93@users.noreply.github.com>
2022-11-10 15:47:28 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 
 import Dropdown from 'react-bootstrap/Dropdown';
 import { logout } from '../services/AuthApi';
@@ -9,28 +9,13 @@ import UserC from '../contexts/UserC';
 import './Navbar.css';
 import React, { useState } from 'react';
 
-import { useNavigate } from "react-router-dom";
-import { getToken } from '../services/AuthApi';
-import { getUser } from '../services/UserApi';
-
 function Navbar() {
     const [click, setClick] = useState(false);
 
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
-
     const {userId, setUserId} = useContext(UserC)
-    const navigate = useNavigate();
-    const [user, setUser]= useState([null]);
-
-    useEffect( ()=> {
-        if(!userId) {
-            navigate("/login", { replace: true })
-        }
-        getUser(userId,getToken())
-            .then(data => setUser(data));
-    },[navigate,userId])
 
     //const navigate = useNavigate();
 
@@ -117,7 +102,7 @@ function Navbar() {
                                                 <path fillRule="evenodd" d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.777 11.777 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7.159 7.159 0 0 0 1.048-.625 11.775 11.775 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.541 1.541 0 0 0-1.044-1.263 62.467 62.467 0 0 0-2.887-.87C9.843.266 8.69 0 8 0zm2.146 5.146a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647z"/>
                                                 </svg>
                                             </button>
-                                            {user.userName}
+                                            User {userId}
                                         </Dropdown.Toggle>
 
                                         <Dropdown.Menu>
