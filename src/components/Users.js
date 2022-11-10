8c5dd20df5bdb736800f46d7c045fd320a636ede
Nabil <nabilkajeiou2000@gmail.com>
2022-11-10 17:48:29 +0100

    format list of users

--- src/components/Users.js
+++ src/components/Users.js
@@ -38,10 +38,19 @@ export default function Users() {
                         <div className='col-sm-3' key={user.id}>
                             <div className="card bg-light" style={{maxWidth: '18rem'}}>
                                 <div className="card-header">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-video" viewBox="0 0 16 16">
-                                        <path d="M8 9.05a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
-                                        <path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2Zm10.798 11c-.453-1.27-1.76-3-4.798-3-3.037 0-4.345 1.73-4.798 3H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-1.202Z"/>
-                                    </svg>{user.email}</div>
+                                    <div class="row">
+                                        <div class="col-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-video" viewBox="0 0 16 16">
+                                                <path d="M8 9.05a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
+                                                <path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2Zm10.798 11c-.453-1.27-1.76-3-4.798-3-3.037 0-4.345 1.73-4.798 3H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-1.202Z"/>
+                                            </svg>
+                                        </div>
+                                        <div class="col-10">
+                                        {user.email}
+                                        </div>
+                                    </div>
+                                    
+                                </div>
                                 <div className="card-body">
                                     <h5 className="card-title">{user.userName}</h5>
                                     
